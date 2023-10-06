84611874147bd622df62ae963af4109fe3ab9833
Nikapwente <hrsmajor@gmail.com>
2023-10-06 15:55:13 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+
+let employee = {
+    name: "Harris Anyangu",
+    streetAdress: "Zambia Road",
+}
+   
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let copyEmployee = {...employee, [key] : value, }
+    return copyEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    let copyEmployee = {...employee, [key]:value }
+    delete copyEmployee[key];
+    return copyEmployee;    
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    console.log(employee);
+    return employee;
+
+}
+
